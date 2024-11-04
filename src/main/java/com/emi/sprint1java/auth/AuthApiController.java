51cd61722fd837c81bf1201c8ee4db25da455d47
LeoSeiti <Leonardoseiti1@gmail.com>
2024-11-04 14:39:13 -0300

    new changes

--- src/main/java/com/emi/sprint1java/auth/AuthApiController.java
+++ src/main/java/com/emi/sprint1java/auth/AuthApiController.java
@@ -1,6 +1,7 @@
 package com.emi.sprint1java.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,4 +16,11 @@ public class AuthController {
     public Token login(@RequestBody Credentials credentials){
         return authService.login(credentials);
     }
+
+
+    @GetMapping("/logout")
+    public String logout(){
+        return "Logout";
+    }
 }
+
